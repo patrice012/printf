4c619ccbad9e3162aaca0ac1002509a5be62e07a
patrice02 <patricedev02@gmail.com>
2023-04-24 21:24:13 +0000

    add more test

--- test1.c
+++ test1.c
@@ -8,12 +8,28 @@
  */
 int main(void)
 {
-	int len = _printf("Let's try to printf a simple sentence.\n");
+	int len = 0, len2 = 0;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+
 
 	_printf("Length:[%d, %i]\n", len, len);
-	_printf("String:[%s]\n", "I am a string !");
+	printf("Length:[%d, %i]\n", len2, len2);
+
 	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	
+
 	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
-	return (1);
+	printf("Len:[%d]\n", len2);
+	_printf("Unknown:[%r]\n");
+	/*printf("Unknown:[%r]\n");*/
+	
+	return (0);
 }
